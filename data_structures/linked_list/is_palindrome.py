cc8055f7525a4f95d9b604434370430af37e3ea8
iamchuckss <s2011297@gmail.com>
2019-07-13 08:39:01 -0500

    add is_palindrome using stack for linked_list

--- data_structures/linked_list/is_palindrome.py
+++ data_structures/linked_list/is_palindrome.py
@@ -2,16 +2,17 @@ def is_palindrome(head):
     if not head:
         return True
     
-    # split the list into two parts
+    # 1. split the list into two parts
     fast, slow = head.next, head
     while fast and fast.next:
         fast = fast.next.next
         slow = slow.next
     
+    # slow will stop at mid if odd, mid-1 if even
     # set second to first node after mid
     second = slow.next
 
-    # reverse second part
+    # 2. reverse second part
     prev = None
     while second:
         temp_next = second.next
@@ -19,7 +20,7 @@ def is_palindrome(head):
         prev = second
         second = temp_next
     
-    # compare reversed second part and first part
+    # 3. compare reversed second part and first part
     # second part has the same or one less node
     second_head = prev
     while second_head:
@@ -28,5 +29,39 @@ def is_palindrome(head):
         second_head = second_head.next
         head = head.next
     return True
+
+def is_palindrome_stack(head):
+    # True if list has only less than 2 nodes
+    if head == None or head.next == None:
+        return True
+
+    # 1. split the list into two parts
+    fast, slow = head.next, head
+    while fast and fast.next:
+        fast = fast.next.next
+        slow = slow.next
+
+    # set second to first node after mid
+    second = slow.next
+    # 2. push the second half into the stack
+    stack = []
+    while second:
+        stack.append(second)
+        second = second.next
+    
+    # 3. comparison
+    while stack:
+        if stack.pop().data != head.data:
+            return False
+        head = head.next
+
+    return True
+
+
+
+    
+    
+
+    
 
 
